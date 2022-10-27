90c626f9c6127bfbe044f7c5e9a21e2d62329be6
Kim Persson <kimpersson88@gmail.com>
2022-10-27 13:17:18 +0800

    feat: add Blowfish API proxy route & global rate limiting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,35 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import pino from "pino-http";
-
+import bodyParser from "body-parser";
+import rateLimit from "express-rate-limit";
+import dotenv from "dotenv";
 dotenv.config();
+
+import { blowfishProxyHandler } from "./routes/proxy";
 
 const port = process.env.PORT ?? "3000";
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute window
+  max: 30, // Limit each IP to 30 requests per `window`
+  standardHeaders: false,
+  legacyHeaders: false,
+});
+
 const app: Express = express();
 app.use(pino());
+// TODO(kimpers): properly validate that origin is "chrome-extension://our_extension_id"
 app.use(cors());
+app.use(limiter);
+app.use(bodyParser.json());
 
-app.get("/*", (req: Request, res: Response) => {
-  const path = req.path;
-  req.log.info(`Hello ${path}`);
-  res.send(`Hello ${path}`);
+// Proxy all request paths starting with /proxy/api/* to Blowfish API
+app.use(`/proxy/api/*`, blowfishProxyHandler);
+app.use("*", (req: Request, res: Response) => {
+  const { path } = req;
+  req.log.warn({ path }, "Invalid path requested");
+  res.send(404);
 });
 
 app.listen(port);
