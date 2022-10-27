5a84feaaaa0995d8f492f5360a543f3b71b53e08
Kim Persson <kimpers@users.noreply.github.com>
2022-10-27 16:25:14 +0800

    feat: setup basic Blowfish API proxy backend (#2)
    * refactor: move extension code into `extension` subfolder
    
    * feat: super basic express server boilerplate
    
    * feat: add Blowfish API proxy route & global rate limiting
    
    * fix: add basic eslint and fix liniting
    
    * fix: update GH action to support extension subdirectory
    
    * chore: add basic GH action CI for backend
    
    * refactor: improve proxy variable naming

---
+++ backend/src/routes/proxy.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import fetch from "node-fetch";
+import qs from "qs";
+
+const { BLOWFISH_API_KEY, BLOWFISH_API_BASE_URL } = process.env;
+
+export const blowfishProxyHandler = async (req: Request, res: Response) => {
+  if (!BLOWFISH_API_KEY) {
+    res.status(500).json({ error: "Missing BLOWFISH_API_KEY" });
+    return;
+  }
+  if (!BLOWFISH_API_BASE_URL) {
+    res.status(500).json({ error: "Missing BLOWFISH_API_BASE_URL" });
+    return;
+  }
+
+  const { body, query, params } = req;
+  const requestedPath = params[0];
+
+  const queryString = qs.stringify(query);
+  const fullQueryString = queryString ? `?${queryString}` : "";
+  const url = `${BLOWFISH_API_BASE_URL}/${requestedPath}${fullQueryString}`;
+  req.log.info({ url }, "Proxying request to Blowfish API");
+
+  try {
+    const proxyResponse = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Api-Key": BLOWFISH_API_KEY,
+      },
+      body: JSON.stringify(body),
+    });
+
+    const responseBody = await proxyResponse.json();
+    res.status(proxyResponse.status).json(responseBody);
+  } catch (err) {
+    req.log.error({ err }, "Error proxying request to Blowfish API");
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
