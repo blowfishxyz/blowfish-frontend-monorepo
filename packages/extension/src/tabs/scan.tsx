09c0065156ca08181209974c5dc3b8c5bf0e1e0c
Kim Persson <kimpersson88@gmail.com>
2023-02-03 16:22:30 +0100

    HACK: diff changes between staging and prod

--- packages/extension/src/tabs/scan.tsx
+++ packages/extension/src/tabs/scan.tsx
@@ -1,3 +1,5 @@
+import { diff } from "deep-object-diff";
+import omit from "lodash/omit";
 import qs from "qs";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 
@@ -25,10 +27,19 @@ import {
   isSignTypedDataRequest,
   parseRequestFromMessage,
 } from "../types";
-import type { ChainFamily, ChainNetwork } from "../utils/BlowfishApiClient";
+import type {
+  ChainFamily,
+  ChainNetwork,
+  EvmExpectedStateChange,
+} from "../utils/BlowfishApiClient";
 import { chainIdToSupportedChainMapping } from "../utils/constants";
 import { logger } from "../utils/logger";
 import { setUnsupportedChainDismissed } from "../utils/storage";
+
+const BLOWFISH_API_STAGING_BASE_URL =
+  process.env.PLASMO_PUBLIC_BLOWFISH_STAGING_API_BASE_URL;
+const BLOWFISH_API_STAGING_API_KEY =
+  process.env.PLASMO_PUBLIC_BLOWFISH_STAGING_API_KEY;
 
 const ScanResult: React.FC = () => {
   const [chainNetwork, setChainNetwork] = useState<ChainNetwork | undefined>(
@@ -74,7 +85,85 @@ const ScanResult: React.FC = () => {
     error: scanError,
     mutate,
     isValidating,
-  } = useScanDappRequest(chainFamily, chainNetwork, request, message?.origin);
+  } = useScanDappRequest(
+    chainFamily,
+    chainNetwork,
+    request,
+    message?.origin,
+    BLOWFISH_API_STAGING_BASE_URL,
+    BLOWFISH_API_STAGING_API_KEY
+  );
+  const { data: scanResultsProd, error: scanErrorProd } = useScanDappRequest(
+    chainFamily,
+    chainNetwork,
+    request,
+    message?.origin
+  );
+
+  useEffect(() => {
+    if (scanError && !scanErrorProd) {
+      console.error(scanError);
+      alert("Staging Error but not prod: " + scanError.message);
+      return;
+    }
+
+    if (!scanError && scanErrorProd) {
+      console.error(scanErrorProd);
+      alert("Prod Error but not staging: " + scanErrorProd.message);
+      return;
+    }
+
+    if (scanResults && scanResultsProd) {
+      logger.debug(
+        "Staging scan results",
+        JSON.stringify(scanResults, undefined, 2)
+      );
+      logger.debug(
+        "Prod scan results",
+        JSON.stringify(scanResultsProd, undefined, 2)
+      );
+
+      const stagingSimulationResults = scanResults.simulationResults;
+      const prodSimulationResults = scanResultsProd.simulationResults;
+      if (stagingSimulationResults?.error !== prodSimulationResults?.error) {
+        alert(
+          "Staging and prod simulation results error property mismatch, please inspect extension logs and report if unexpected"
+        );
+        return;
+      }
+
+      const cleanStateChange = (stateChange: EvmExpectedStateChange) =>
+        omit(stateChange, ["rawInfo.data.asset"]);
+
+      const stagingCleanedStateChanges =
+        stagingSimulationResults?.expectedStateChanges?.map(cleanStateChange) ??
+        [];
+      const prodCleanedStateChanges =
+        prodSimulationResults?.expectedStateChanges?.map(cleanStateChange) ??
+        [];
+
+      const maxStateChangesLength = Math.max(
+        stagingCleanedStateChanges.length,
+        prodCleanedStateChanges.length
+      );
+
+      for (let i = 0; i < maxStateChangesLength; i++) {
+        const currDiff = diff(
+          stagingCleanedStateChanges[i],
+          prodCleanedStateChanges[i]
+        );
+        if (Object.keys(currDiff).length !== 0) {
+          logger.error(
+            "State change diff between staging and prod",
+            JSON.stringify(currDiff, undefined, 2)
+          );
+          alert(
+            "Staging and prod scan results do not match! Please inspect extension logs for differences and report if unexpected"
+          );
+        }
+      }
+    }
+  }, [scanResults, scanResultsProd, scanErrorProd, scanError]);
 
   // Reset-retry state when we are no longer validating
   useEffect(() => {
