f86d0126533ce33e0657c3999399908d2ed003aa
Kim Persson <kimpersson88@gmail.com>
2023-03-27 22:48:24 +0200

    fix: Use same providers for portal page and storybook

--- apps/transaction-portal/src/components/Providers.tsx
+++ apps/transaction-portal/src/components/Providers.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { WagmiConfig } from "wagmi";
+import { GlobalStyle } from "../styles/global";
+import { wagmiClient } from "../utils/wagmi";
+import { ConnectKitProvider } from "connectkit";
 
 import { themes } from "../styles/theme";
 
 // NOTE: All global providers should be added here
 export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <ThemeProvider theme={themes.light}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={themes.light}>
+      <GlobalStyle />
+      <WagmiConfig client={wagmiClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </WagmiConfig>
+    </ThemeProvider>
+  );
 };
--- apps/transaction-portal/src/pages/_app.tsx
+++ apps/transaction-portal/src/pages/_app.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "styled-components";
-import { WagmiConfig } from "wagmi";
-import { GlobalStyle } from "../styles/global";
-import { themes } from "../styles/theme";
-import { wagmiClient } from "../utils/wagmi";
-import { ConnectKitProvider, ConnectKitButton } from "connectkit";
+import { Providers } from "../components/Providers";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -14,15 +9,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Blowfish</title>
       </Head>
-      <ThemeProvider theme={themes.light}>
-        <GlobalStyle />
-        <WagmiConfig client={wagmiClient}>
-          <ConnectKitProvider>
-            <Component {...pageProps} />
-            <ConnectKitButton />
-          </ConnectKitProvider>
-        </WagmiConfig>
-      </ThemeProvider>
+      <Providers>
+        <Component {...pageProps} />
+      </Providers>
     </>
   );
 }
