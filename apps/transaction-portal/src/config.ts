89d6b1c9962ba8167b5f7af4ebfc67a3cb15442d
tzoeov <andreitoev@gmail.com>
2023-03-31 16:55:15 +0300

    chore: remove throw error on missing Alchemy API key

--- apps/transaction-portal/src/config.ts
+++ apps/transaction-portal/src/config.ts
@@ -4,6 +4,3 @@ export const CHROMIMUM_INSTALL_EXTENSION_URL = process.env
   .NEXT_PUBLIC_CHROMIMUM_INSTALL_EXTENSION_URL as string;
 export const ALCHEMY_API_KEY = process.env
   .NEXT_PUBLIC_ALCHEMY_API_KEY as string;
-if (!ALCHEMY_API_KEY) {
-  throw new Error("Missing ALCHEMY_API_KEY");
-}
