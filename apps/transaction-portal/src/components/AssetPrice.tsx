ce6f6eb6c21785d0cd274f35f39eb89f23fab5c0
tzoeov <andreitoev@gmail.com>
2023-04-02 15:24:10 +0300

    chore: improve comment

--- apps/transaction-portal/src/components/AssetPrice.tsx
+++ apps/transaction-portal/src/components/AssetPrice.tsx
@@ -12,7 +12,7 @@ interface AssetPriceProps {
 }
 
 const AssetPrice = ({ stateChange }: AssetPriceProps) => {
-  //TODO: remove "any" once the API is transitioned away from assetPrice
+  //TODO: refactor price once the API is transitioned away from assetPrice
   const price =
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (stateChange.data as any)?.asset?.price?.dollar_value_per_token ??
