2ee8df09436a777ca4c3660e1a24559d42b44867
Oyinlolu <oyinloluwab@gmail.com>
2023-05-11 18:05:56 -0400

    feat: protect v2 loading screen

--- apps/transaction-portal/src/components/ProtectHomeScreen.tsx
+++ apps/transaction-portal/src/components/ProtectHomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { styled } from "styled-components";
 import ProtectHeader from "./ProtectHeader";
+import ProtectLoadingScreen from "./ProtectLoadingScreen";
 
 const ProtectScreenWrapper = styled.div`
   padding: 40px;
@@ -8,10 +9,21 @@ const ProtectScreenWrapper = styled.div`
   background: #efefef;
 `;
 
+const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const ProtectHomeScreen = () => {
   return (
     <ProtectScreenWrapper>
       <ProtectHeader />
+      <LoadingWrapper>
+        <ProtectLoadingScreen />
+      </LoadingWrapper>
     </ProtectScreenWrapper>
   );
 };
---
+++ apps/transaction-portal/src/components/ProtectLoadingScreen.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { CardText, CardWrapper } from "./cards/common";
+import { styled } from "styled-components";
+import { Column, TextXL } from "@blowfish/ui/core";
+import { BlowfishIcon } from "@blowfish/ui/icons";
+
+const ProtectLoadingScreenContainer = styled(CardWrapper)`
+  flex: unset;
+  width: 370px;
+`;
+
+const StyledBlowfishIcon = styled(BlowfishIcon)`
+  width: 104px;
+  height: 102px;
+  opacity: 0.2;
+`;
+
+const ProtectLoadingScreenTitle = styled(TextXL)`
+  margin: 8px 0;
+`;
+
+const ProtectLoadingScreenMessage = styled(CardText)`
+  width: 250px;
+  text-align: center;
+  font-size: 15px;
+  line-height: 21px;
+  letter-spacing: -0.01em;
+`;
+
+const ProtectLoadingScreen = () => {
+  return (
+    <ProtectLoadingScreenContainer>
+      <Column gap="md" alignItems="center">
+        <StyledBlowfishIcon />
+        <ProtectLoadingScreenTitle>Simulating...</ProtectLoadingScreenTitle>
+        <ProtectLoadingScreenMessage>
+          We are making sure that it is safe for you to proceed with this
+          transaction.
+        </ProtectLoadingScreenMessage>
+      </Column>
+    </ProtectLoadingScreenContainer>
+  );
+};
+
+export default ProtectLoadingScreen;
