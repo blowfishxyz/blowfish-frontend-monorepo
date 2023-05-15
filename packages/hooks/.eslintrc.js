6fc2d799851aec6d361fae9d9734c2dffd361070
Kim Persson <kimpers@users.noreply.github.com>
2023-05-15 16:35:09 +0200

    feat(extension-landing): Add Matomo analytics in prod (#87)

---
+++ packages/hooks/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint"],
+  root: true,
+};
