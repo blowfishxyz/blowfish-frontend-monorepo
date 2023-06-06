643e72c442e26e49cea79c91e8b05b6432bb4ec7
Oyinlolu <oyinloluwab@gmail.com>
2023-06-06 14:21:22 -0400

    refactor: modified PreviewTxn storybook component

--- apps/transaction-portal/src/stories/PreviewTxn.stories.tsx
+++ apps/transaction-portal/src/stories/PreviewTxn.stories.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Story, Meta } from "@storybook/react";
 import { PreviewTxnProps, PreviewTxn } from "~components/cards/PreviewTxn";
 import { sendErc721 } from "~components/fixtures/state-changes";
+import { CardContent } from "~components/cards/common";
+import { Text } from "@blowfish/ui/core";
+import { ArrowDownIcon } from "@blowfish/ui/icons";
+import { Row } from "@blowfish/ui/core";
+import { ConfirmTxn } from "~components/cards/ConfirmTxn";
 
 export default {
   title: "Components/PreviewTxn",
@@ -18,6 +23,32 @@ TransactionPreview.args = {
     dappUrl: new URL("https://www.blur.io"),
     account: "0xD854343f41B2138B686F2D3bA38402A9F7Fb4337",
   },
+  advancedDetails: () => (
+    <>
+      <CardContent>
+        <Row
+          justifyContent="space-between"
+          alignItems="center"
+          marginBottom={16}
+          onClick={() => {
+            console.log("clicked");
+          }}
+        >
+          <Text design="secondary" size="sm">
+            View more details
+          </Text>
+          <ArrowDownIcon />
+        </Row>
+        <ConfirmTxn
+          onContinue={() => console.log("clicked")}
+          onCancel={() => console.log("clicked")}
+          onReport={() => console.log("clicked")}
+          severity="INFO"
+          warnings={undefined}
+        />
+      </CardContent>
+    </>
+  ),
 };
 
 export const SignaturePreview = Template.bind({});
@@ -32,4 +63,30 @@ SignaturePreview.args = {
       account: "0xD854343f41B2138B686F2D3bA38402A9F7Fb4337",
     },
   ],
+  advancedDetails: () => (
+    <>
+      <CardContent>
+        <Row
+          justifyContent="space-between"
+          alignItems="center"
+          marginBottom={16}
+          onClick={() => {
+            console.log("clicked");
+          }}
+        >
+          <Text design="secondary" size="sm">
+            View more details
+          </Text>
+          <ArrowDownIcon />
+        </Row>
+        <ConfirmTxn
+          onContinue={() => console.log("clicked")}
+          onCancel={() => console.log("clicked")}
+          onReport={() => console.log("clicked")}
+          severity="INFO"
+          warnings={undefined}
+        />
+      </CardContent>
+    </>
+  ),
 };
