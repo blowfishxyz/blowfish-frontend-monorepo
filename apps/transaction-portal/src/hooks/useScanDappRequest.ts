7090815a1521a258cff687e2bd328aae4b208fb9
Oyinlolu <oyinloluwab@gmail.com>
2023-08-04 12:07:43 -0400

    refactor: handle errors when polling api (#144)
    * refactor: handle errors when polling api
    
    * refactor: handle errors when polling the api
    
    * refactor: addressed pr comments
    
    * refactor: moved the poll error handling logic to useScanDappRequest
    
    * refactor: debugging polling error
    
    * refactor: handled all cases for polling error bug
    
    * refactor: fixed formatting error
    
    * refactor: changed SCAN_REFRESH_INTERVAL_MS back to 15_000

--- apps/transaction-portal/src/hooks/useScanDappRequest.ts
+++ apps/transaction-portal/src/hooks/useScanDappRequest.ts
@@ -17,6 +17,7 @@ import {
 } from "@blowfish/utils/types";
 import useSWR, { SWRResponse } from "swr";
 import { isSmartContractWallet } from "../utils/wallets";
+import { useRef } from "react";
 
 export const BLOWFISH_API_BASE_URL = process.env
   .NEXT_PUBLIC_BLOWFISH_API_BASE_URL as string;
@@ -100,13 +101,33 @@ export const useScanDappRequest = (
   request: DappRequest | undefined,
   origin: string | undefined
 ): SWRResponse<EvmTransactionScanResult | EvmMessageScanResult, Error> => {
-  return useSWR(
+  const prevResponseRef = useRef<SWRResponse<
+    EvmTransactionScanResult | EvmMessageScanResult
+  > | null>(null);
+  const consecutiveErrorCountRef = useRef<number>(0);
+
+  const response = useSWR(
     getCacheKey(chainFamily, chainNetwork, request, origin),
     (params) => fetcher(...params),
     {
       refreshInterval: SCAN_REFRESH_INTERVAL_MS,
     }
   );
+
+  if (response.error) {
+    consecutiveErrorCountRef.current += 1;
+
+    if (consecutiveErrorCountRef.current <= 2 && prevResponseRef.current) {
+      return prevResponseRef.current;
+    }
+
+    return response;
+  }
+
+  consecutiveErrorCountRef.current = 0;
+  prevResponseRef.current = response ?? null;
+
+  return response;
 };
 
 function mapTransactionsToSingle(
