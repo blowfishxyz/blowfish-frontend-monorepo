24cf0340be8777f1f8953ce1acdce4d60ba74da9
Kim Persson <kimpersson88@gmail.com>
2023-08-31 20:12:55 +0200

    fix: add CSP policy for iframes

--- apps/extension-landing-page/next.config.js
+++ apps/extension-landing-page/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  frame-ancestors 'self';
+`;
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
@@ -7,6 +12,17 @@ const nextConfig = {
       displayName: true,
     },
   },
+  headers: async () => [
+    {
+      source: "/(.*?)",
+      headers: [
+        {
+          key: "Content-Security-Policy",
+          value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
--- apps/transaction-portal/next.config.js
+++ apps/transaction-portal/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  frame-ancestors 'self';
+`;
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
@@ -25,10 +30,20 @@ const nextConfig = {
       },
     ];
   },
+  headers: async () => [
+    {
+      source: "/(.*?)",
+      headers: [
+        {
+          key: "Content-Security-Policy",
+          value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
-
 
 // Injected content via Sentry wizard below
 
