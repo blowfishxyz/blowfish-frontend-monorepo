ffb6857a800e2d1d25beb9d69466e10b1ef3dd40
Alexandr Metreniuc <metreniuk@users.noreply.github.com>
2023-10-18 17:56:41 +0300

    feat: fetch transaction data by txn hash (#161)

---
+++ apps/transaction-portal/src/hooks/useSimulateByTxnHash.ts
@@ -0,0 +1,37 @@
+import { EvmSimulatorConfig } from "@blowfishxyz/api-client/v20230605";
+import { useRouter } from "next/router";
+import { useClient } from "wagmi";
+import { UrlParsedRequest } from "./useURLRequestParams";
+
+export function useSimulateByTxnHash() {
+  const router = useRouter();
+  const client = useClient();
+
+  return async (txnHash: string, domain: string) => {
+    const data = await client.getProvider().getTransaction(txnHash);
+    if (!data.blockNumber) {
+      return;
+    }
+    const dataToSend: UrlParsedRequest = {
+      metadata: { origin: domain },
+      userAccount: data.from,
+      txObjects: [
+        {
+          from: data.from,
+          to: data.to,
+          data: data.data,
+        },
+      ],
+    };
+    const simulatorConfig: EvmSimulatorConfig = {
+      blockNumber: (data.blockNumber - 1).toString(),
+    };
+    router.push(
+      `/v2/simulate?request=${encodeURIComponent(
+        JSON.stringify(dataToSend)
+      )}&chainId=${data.chainId}&simulatorConfig=${encodeURIComponent(
+        JSON.stringify(simulatorConfig)
+      )}`
+    );
+  };
+}
