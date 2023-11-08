83b0e6fdab59a760a2ae652d920dfe730d0f7dfe
Alexandr Metreniuc <metreniuk@gmail.com>
2023-11-08 13:31:46 +0200

    move api client to a context

---
+++ apps/transaction-portal/src/hooks/useClient.tsx
@@ -0,0 +1,38 @@
+import { createContext, memo, useContext, useState } from "react";
+import { createEvmClient, BlowfishEvmApiClient } from "@blowfishxyz/api-client";
+import { ChainFamily, ChainNetwork } from "@blowfish/utils/chains";
+
+export const BLOWFISH_API_BASE_URL = process.env
+  .NEXT_PUBLIC_BLOWFISH_API_BASE_URL as string;
+
+const ApiClientContext = createContext<BlowfishEvmApiClient | null>(null);
+
+export const ApiClientProvider = memo(function ApiClientProvider({
+  chainFamily,
+  chainNetwork,
+  children,
+}: React.PropsWithChildren<{
+  chainFamily: ChainFamily;
+  chainNetwork: ChainNetwork;
+}>) {
+  const [value] = useState(() =>
+    createEvmClient({
+      basePath: BLOWFISH_API_BASE_URL,
+      chainFamily,
+      chainNetwork,
+    })
+  );
+  return (
+    <ApiClientContext.Provider value={value}>
+      {children}
+    </ApiClientContext.Provider>
+  );
+});
+
+export function useClient() {
+  const client = useContext(ApiClientContext);
+  if (!client) {
+    throw new Error("useClient must be used within a ApiClientProvider");
+  }
+  return client;
+}
