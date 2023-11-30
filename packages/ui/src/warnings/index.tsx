d0f9ef5c3042b73b3b48aa2bde04ea8e61ed045b
Oyinlolu <oyinloluwab@gmail.com>
2023-11-30 13:36:06 -0500

    refactor: handle warnings for simulation warning (#178)
    * refactor: handle warnings for simulation warning
    
    * refactor: addressed pr comment

--- packages/ui/src/warnings/index.tsx
+++ packages/ui/src/warnings/index.tsx
@@ -42,6 +42,8 @@ export const SimulationWarning: React.FC<{ warning: UIWarning }> = ({
         return "Eth sign transaction hash";
       case WarningInnerKindEnum.Forta:
         return "Potentially malicious on the Forta Network";
+      case WarningInnerKindEnum.ImbalancedDollarValue:
+        return "Imbalanced Dollar Value";
       case WarningInnerKindEnum.KnownMalicious:
         return "Known malicious";
       case WarningInnerKindEnum.MaliciousPackages:
@@ -68,6 +70,8 @@ export const SimulationWarning: React.FC<{ warning: UIWarning }> = ({
         return "Too many transactions";
       case WarningInnerKindEnum.TradeForNothing:
         return "Trade for nothing";
+      case WarningInnerKindEnum.TradeForUnverifiedNft:
+        return "Trade for unverified NFT";
       case WarningInnerKindEnum.TransferringErc20ToOwnContract:
         return "Transferring ERC20 to own contract";
       case WarningInnerKindEnum.TransferringTooMuchSol:
@@ -99,6 +103,9 @@ export const SimulationWarning: React.FC<{ warning: UIWarning }> = ({
       case WarningInnerKindEnum.CompromisedAuthorityUpgrade:
       case WarningInnerKindEnum.SetOwnerAuthority:
       case WarningInnerKindEnum.UserAccountOwnerChange:
+
+      case WarningInnerKindEnum.BlurBulkOrderNotOnBlur:
+      case WarningInnerKindEnum.BlurV2OrderNotOnBlur:
         return "user";
       case WarningInnerKindEnum.CopyCatDomain:
       case WarningInnerKindEnum.MultiCopyCatDomain:
@@ -113,6 +120,7 @@ export const SimulationWarning: React.FC<{ warning: UIWarning }> = ({
       case WarningInnerKindEnum.TooManyTransactions:
       case WarningInnerKindEnum.TradeForNothing:
       case WarningInnerKindEnum.NewDomain:
+      case WarningInnerKindEnum.TradeForUnverifiedNft:
         return "question-mark";
       case WarningInnerKindEnum.BlocklistedDomainCrossOrigin:
       case WarningInnerKindEnum.BulkApprovalsRequest:
@@ -127,8 +135,13 @@ export const SimulationWarning: React.FC<{ warning: UIWarning }> = ({
       case WarningInnerKindEnum.WhitelistedDomainCrossOrigin:
       case WarningInnerKindEnum.ReferencedOfacAddress:
       case WarningInnerKindEnum.UnusualGasConsumption:
-      case WarningInnerKindEnum.PoisonedAddress:
       case WarningInnerKindEnum.DurableNonce:
+      case WarningInnerKindEnum.Forta:
+      case WarningInnerKindEnum.MaliciousPackages:
+      case WarningInnerKindEnum.TransferringTooMuchSol:
+      case WarningInnerKindEnum.TransfersMajorityOfYourSol:
+      case WarningInnerKindEnum.YakoaNftIpInfringement:
+      case WarningInnerKindEnum.ImbalancedDollarValue:
       default:
         return "exclamation";
     }
