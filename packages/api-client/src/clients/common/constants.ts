df536d469ea8c7f62c86f460212cb33b4770388f
Alexandr Metreniuc <metreniuk@gmail.com>
2024-03-18 10:54:09 +0200

    bump versions

--- packages/api-client/src/clients/common/constants.ts
+++ packages/api-client/src/clients/common/constants.ts
@@ -2,5 +2,5 @@ export type HTTPHeaders = { [key: string]: string };
 
 export const BASE_HEADERS: HTTPHeaders = {
   ["Content-Type"]: "application/json",
-  ["X-Api-Client-Version"]: "0.1.12",
+  ["X-Api-Client-Version"]: "0.1.13",
 };
