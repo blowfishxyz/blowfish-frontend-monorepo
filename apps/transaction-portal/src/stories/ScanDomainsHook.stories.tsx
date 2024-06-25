a89b137ee1163529bf8f9d0d5fb5f5a0dc0597bd
Alexandr Metreniuc <metreniuk@gmail.com>
2024-06-25 13:40:39 +0300

    fix crashes

--- apps/transaction-portal/src/stories/ScanDomainsHook.stories.tsx
+++ apps/transaction-portal/src/stories/ScanDomainsHook.stories.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Meta } from "@storybook/react";
-import { useScanDomain } from "@blowfishxyz/ui";
+import { useScanDomains } from "@blowfishxyz/ui";
 import { JsonViewer } from "~components/JsonViewerV2";
 import { ProtectLoadingScreen } from "~components/ProtectLoadingScreen";
 import { TransactionNotFoundModal } from "~components/modals";
 
 export default {
-  title: "Hooks/useScanDomain",
+  title: "Hooks/useScanDomains",
   component: JsonViewer,
 } as Meta;
 export const ScanDomain = () => {
-  const { data, isLoading } = useScanDomain([
+  const { data, isLoading } = useScanDomains([
     "https://app.uniswap.org",
     "https://opensea.io",
   ]);
