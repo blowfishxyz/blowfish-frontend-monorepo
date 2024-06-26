549cde81efe4fd9126bc8b6ff888335ba85dcaf6
Alexandr Metreniuc <metreniuk@gmail.com>
2024-06-26 13:23:43 +0300

    add comment

--- apps/extension/src/utils/decode.ts
+++ apps/extension/src/utils/decode.ts
@@ -1,10 +1,6 @@
 import bs58 from "bs58";
 
-/**
- * Decode a raw transaction
- * Either a base58 or base64 encoded transaction
- * @param {string} rawTx
- */
+// Note: Use @blowfishxyz/safeguard when merged
 export function decodeRawTransaction(rawTx: string): Uint8Array {
   return /[0OIl+/]/.test(rawTx)
     ? new Uint8Array(Buffer.from(rawTx, "base64"))
