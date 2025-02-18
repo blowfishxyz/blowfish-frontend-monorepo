412f670cc551b7a29f436d323492f0050af7f821
Kim Persson <kim@phantom.app>
2025-02-18 16:16:00 +0000

    chore: make ready for open sourcing
    * remove old revoked api keys
    * remove deprecated landing page
    * add license
    * remove unnecessary GH actions

--- apps/extension-landing-page/src/config.ts
+++
@@ -1,3 +0,0 @@
-export const CHROME_EXTENSION_STORE_URL =
-  "https://chrome.google.com/webstore/detail/blowfish-wallet-protect-b/kbjhmlgclljgdhmhffjofbobmficicjp";
-export const MATOMO_SITE_ID = "1";
